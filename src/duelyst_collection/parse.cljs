81452c3def79b8877a645eb4812fc2008862a35b
jrheard <jrheard@cs.stanford.edu>
2016-12-18 14:07:07 -0800

    add dev diary, notes

--- src/duelyst_collection/parse.cljs
+++ src/duelyst_collection/parse.cljs
@@ -4,14 +4,14 @@
     [clojure.spec :as s]
     [clojure.string :refer [split lower-case replace]]))
 
-; here's an example card:
+; here's an example card from json:
 ; {
 ; "description": "Turn a 2x2 area into Shadow Creep.",
 ; "faction": "Abyssian Host",
-; "race": "Spell",
+; "race": "Spell", <---- ????????
 ; "name": "Shadow Nova",
 ; "lastModifed": 1.71,
-; "type": "Spell",
+; "type": "Spell", <----- ????? dupe?
 ; "created": 0.01,
 ; "manaCost": 4,
 ; "id": 20051,
@@ -19,6 +19,18 @@
 ; "rarity": "Basic"
 ; }
 
+; here's an example card csv line:
+; headers ["Count", "Name", "Faction", "Rarity", "Prismatic", "Cost", "DE Value"]
+; "0","Snow Rippler","Vanar","Common","false","40","10"
+; note that set isn't included
+
+; ok so are there two different kinds of cards?
+; there's a canonical card
+; and a collection card
+; seems gross
+; what if we merge the two sources of truth into one?
+; so we parse the collection first so we can get a mapping of name -> id
+; and ::prismatic is an optional key
 
 (s/def ::card (s/keys :req [::count ::name ::faction ::rarity ::pristmatic ::cost ::de-value]))
 
@@ -32,7 +44,7 @@
    [:de-value js/parseInt]])
 
 (defn parse-csv-card-line [card-line]
-  ;(js/console.log card-line)
+  (js/console.log card-line)
 
   (let [fields (-> card-line
                    (subs 1 (- (count card-line)
