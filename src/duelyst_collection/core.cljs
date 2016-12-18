e8f08c971e028d841601e43aeadec5ebbaaef1da
jrheard <jrheard@cs.stanford.edu>
2016-12-17 16:56:02 -0800

    displaying cards

--- src/duelyst_collection/core.cljs
+++ src/duelyst_collection/core.cljs
@@ -1,15 +1,29 @@
 (ns duelyst-collection.core
-  (:require [reagent.core :as r]))
+  (:require [ajax.core :refer [GET]]
+            [reagent.core :as r]))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn make-app-state []
+  {:all-cards []})
+
+(defonce app-state (r/atom (make-app-state)))
 
 (defn foo []
-  [:h2 "bar"]
+  (js/console.log (first (@app-state :all-cards)))
+  [:ul
+   (for [card (@app-state :all-cards)]
+     ^{:key (card :id)} [:li (card :name)]
+     )
+   ]
+
 
 
   )
 
 (defn ^:export main []
+  (GET "/all_cards.json" {:handler #(swap! app-state assoc :all-cards %)
+                          :response-format :json
+                          :keywords? true})
+
   (r/render-component [foo]
                       (js/document.getElementById "app")))
 
