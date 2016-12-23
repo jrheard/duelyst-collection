5368db02eb3974e33cb2e905e55ca1a33ea485b5
jrheard <jrheard@cs.stanford.edu>
2016-12-23 16:19:18 -0600

    completion breakdown by faction

--- src/duelyst_collection/html.cljs
+++ src/duelyst_collection/html.cljs
@@ -1,18 +1,42 @@
 (ns duelyst-collection.html
-  (:require [duelyst-collection.card-list :as card-list]
-            [duelyst-collection.collection :as collection]))
+  (:require [clojure.spec :as s]
+            [duelyst-collection.card-list :as card-list]
+            [duelyst-collection.collection :as collection]
+            [duelyst-collection.specs :as specs]))
+
+(defn format-percentage [percentage]
+  (int (* percentage 100)))
 
 (defn render-card-list [cards]
   [:ul
    (for [card cards]
      ^{:key (card :card/id)} [:li (card :card/name)])])
 
-(defn cards-you-own [app-state]
-  (render-card-list (map :card/card (@app-state :my-cards))))
+(defn cards-you-own [cards]
+  (render-card-list (map :card/card cards)))
 
-(defn missing-cards [app-state]
-  (let [missing (->> @app-state
-                     :my-cards
+(defn completion [cards]
+  (js/console.log (s/form :card/faction))
+  [:div.completion
+   [:h1 "collection completion:"]
+   [:p (str "overall: "
+            (format-percentage (collection/completion-percentage cards))
+            "%")]
+   (for [faction (s/form :card/faction)]
+     (let [faction-cards (filter #(= (-> % :card/card :card/faction)
+                                     faction)
+                                 cards)]
+
+       ^{:key faction}
+       [:p (str faction
+                ": "
+                (format-percentage
+                  (/ (apply + (map :collection/count faction-cards))
+                     (* 3 (count faction-cards))))
+                "%")]))])
+
+(defn missing-cards [cards]
+  (let [missing (->> cards
                      collection/missing-cards
                      (sort-by :collection/count)
                      reverse)]
@@ -25,12 +49,11 @@
         [:span.missing-count (str " " (- 3 (card :collection/count)))]])]))
 
 (defn render-app [app-state]
-  [:div
-   [:h1 "collection completion:"]
-   [:p (str (int (collection/completion-percentage (@app-state :my-cards)))
-            "%")]
-   [:h1 "your collection"]
-   [cards-you-own app-state]
-   [:h1 "missing cards (card name: number missing)"]
-   [missing-cards app-state]])
+  (let [cards (@app-state :my-cards)]
+    [:div
+     [completion cards]
+     [:h1 "your collection"]
+     [cards-you-own cards]
+     [:h1 "missing cards (card name: number missing)"]
+     [missing-cards cards]]))
 
