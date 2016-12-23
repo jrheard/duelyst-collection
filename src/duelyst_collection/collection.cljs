bb4b3ea35f50c2e33f7b8319efc2deeb3b69368a
jrheard <jrheard@cs.stanford.edu>
2016-12-23 16:34:09 -0600

    oops, add duelyst-collection.collection

---
+++ src/duelyst_collection/collection.cljs
@@ -0,0 +1,19 @@
+(ns duelyst-collection.collection
+  (:require [clojure.spec :as s]
+            [duelyst-collection.specs]))
+
+(defn missing-cards [cards]
+  (filter #(< (% :collection/count) 3)
+          cards))
+
+(s/fdef missing-cards
+  :args (s/cat :cards (s/coll-of :collection/card))
+  :ret (s/coll-of :collection/card))
+
+(defn completion-percentage [cards]
+  (/ (apply + (map :collection/count cards))
+     (* 3 (count cards))))
+
+(s/fdef completion-percentage
+  :args (s/cat :cards (s/coll-of :collection/card))
+  :ret number?)
